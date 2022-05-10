0372892732653b6eef64fe88aad6546f052d9b51
Laura Zumbrunnen <zumbrun@gmail.com>
2022-05-10 11:02:41 -0500

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) return "OOPS";
+    let prevsum = 0;
+    let currentsum = 1;
+    let newsum = currentsum + prevsum;
+    console.log({prevsum},{currentsum});
+    for (let i = 1; i < a; i++) {
+        newsum = currentsum + prevsum;
+        prevsum = currentsum;
+        currentsum = newsum;
+        //console.log({prevsum},{currentsum}, {newsum});
+    }
+    return newsum;
 };
 
 // Do not edit below this line
