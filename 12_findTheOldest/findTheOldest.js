56abd3b6b71601bfd09373e3998f9625b275f1a4
Laura Zumbrunnen <zumbrun@gmail.com>
2022-05-10 13:06:54 -0500

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort(function(a,b) {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        console.log({aAge},{bAge});
+        return aAge > bAge ? 1 : -1;
+    });
+    return people.pop();
 };
 
 // Do not edit below this line
