74a81f7e213193118dab09449b9132a1f5b07f2c
Laura Zumbrunnen <zumbrun@gmail.com>
2022-05-10 10:22:03 -0500

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    const strippedStr = str.replace(/[^a-zA-Z]/g,"")
+    const lowerStr = strippedStr.toLowerCase();
+    //console.log({lowerStr});
+    const strArray = lowerStr.split("");
+    //console.log({strArray});
+    const revArray = strArray.reverse();
+    //console.log({revArray});
+    const joinStr = revArray.join("");
+    console.log({joinStr}, {lowerStr});
+    return joinStr === lowerStr ? true : false;
 };
 
 // Do not edit below this line
